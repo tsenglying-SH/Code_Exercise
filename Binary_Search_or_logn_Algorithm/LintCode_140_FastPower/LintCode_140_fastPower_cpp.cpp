4c366405be0a745b824b6c05e70f11abe1607ceb
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-03 18:56:49 +0800

    '%' operator method, and avoid overflow

---
+++ Binary_Search_or_logn_Algorithm/LintCode_140_FastPower/LintCode_140_fastPower_cpp.cpp
@@ -0,0 +1,31 @@
+// When using cpp, pay attention to int overflow. Time O(logn)
+class Solution {
+public:
+	/**
+	* @param a: A 32bit integer
+	* @param b: A 32bit integer
+	* @param n: A 32bit integer
+	* @return: An integer
+	*/
+	int fastPower(int a, int b, int n) {
+		// write your code here
+		if (n == 0 || a == 1) {
+			return 1 % b;
+		}
+		if (a == 0) {
+			return 0;
+		}
+
+		long res = 1 % b;
+		long tmp = a;
+		while (n > 0) {
+			if (n & 1) {
+				res = ((res % b) * (tmp % b)) % b;
+			}
+
+			tmp = (tmp % b) * (tmp % b);
+			n >>= 1;
+		}
+		return res % b;
+	}
+};
