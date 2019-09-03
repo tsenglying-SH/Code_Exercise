1948dfa3e021c9943b970945aacb18727f65e319
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-03 19:50:35 +0800

    Nothing Special

---
+++ Binary_Search_or_logn_Algorithm/Lintcode_75_Leetcode_162_FindPeakElement/Lintcode_75_Leetcode_162_FindPeakElement_cpp.cpp
@@ -0,0 +1,27 @@
+// binary search time O(logn)
+class Solution {
+public:
+	/**
+	* @param A: An integers array.
+	* @return: return any of peek positions.
+	*/
+	int findPeak(vector<int> &A) {
+		// write your code here
+		if (A.empty()) {
+			return -1;
+		}
+
+		int start = 0, end = A.size() - 1;
+
+		while (start + 1 <  end) {
+			int mid = (end - start) / 2 + start;
+			if (A[mid] > A[mid + 1]) {
+				end = mid;
+			}
+			else {
+				start = mid;
+			}
+		}
+		return A[start] > A[end] ? start : end;
+	}
+};
