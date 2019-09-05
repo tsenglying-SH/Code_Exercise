e489779405cc8db64db0f6bf0bfb30afa810433a
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-05 18:52:54 +0800

    Add files via upload

---
+++ twoPointers/Lincode_539_Leetcode_283_MoveZeroes/LintCode_539_Leetcode_283_MoveZeroes_cpp.cpp
@@ -0,0 +1,34 @@
+// do write opreation as few as possible
+class Solution {
+public:
+	/**
+	* @param nums: an integer array
+	* @return: nothing
+	*/
+	void moveZeroes(vector<int> &nums) {
+		// write your code here
+		if (nums.empty()) {
+			return;
+		}
+
+		int slow = 0, fast = 0;
+
+		while (fast < nums.size()) {
+			if (nums[fast] != 0) {
+				if (fast != slow) {
+					nums[slow] = nums[fast];
+				}
+				slow++;
+			}
+			fast++;
+		}
+
+		while (slow < nums.size()) {
+			if (nums[slow] != 0) {
+				nums[slow] = 0;
+			}
+			slow++;
+		}
+
+	}
+};
