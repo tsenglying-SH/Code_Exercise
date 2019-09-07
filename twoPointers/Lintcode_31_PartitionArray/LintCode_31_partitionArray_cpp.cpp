5e0037daf5dc21fea9c5884131cf98e7a2d153f6
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-07 17:21:37 +0800

    Add files via upload

---
+++ twoPointers/Lintcode_31_PartitionArray/LintCode_31_partitionArray_cpp.cpp
@@ -0,0 +1,29 @@
+// O(n) partition, the base of Quick Sort
+class Solution {
+public:
+	/**
+	* @param nums: The integer array you should partition
+	* @param k: An integer
+	* @return: The index after partition
+	*/
+	int partitionArray(vector<int> &nums, int k) {
+		// write your code here
+		if (nums.empty()) {
+			return 0;
+		}
+
+		int l = 0, r = nums.size() - 1;
+		while (l <= r) {
+			while (l <= r && nums[l] < k) {
+				l++;
+			}
+			while (l <= r && nums[r] >= k) {
+				r--;
+			}
+			if (l <= r) {
+				swap(nums[l++], nums[r--]);
+			}
+		}
+		return l;
+	}
+};
