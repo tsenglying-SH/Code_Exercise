fe9d07804c72143d9b3dbdc6c5f54490136427cc
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-14 14:31:41 +0800

    piggy back on calculating the sum of tree

---
+++ Binary_Tree/LintCode_596_findSubtree/LintCode_596_findSubtree_cpp.cpp
@@ -0,0 +1,41 @@
+// Calculate the sum of tree and upgrate the minimum subtree along the way
+
+/**
+* Definition of TreeNode:
+* class TreeNode {
+* public:
+*     int val;
+*     TreeNode *left, *right;
+*     TreeNode(int val) {
+*         this->val = val;
+*         this->left = this->right = NULL;
+*     }
+* }
+*/
+
+class Solution {
+private:
+	int minimum = INT_MAX;
+	TreeNode * minimum_root = nullptr;
+public:
+	/**
+	* @param root: the root of binary tree
+	* @return: the root of the minimum subtree
+	*/
+	TreeNode * findSubtree(TreeNode * root) {
+		// write your code here
+		if (root == nullptr) { return root; }
+		sum_tree(root);
+		return minimum_root;
+	}
+
+	int sum_tree(TreeNode * root) {
+		if (root == nullptr) { return 0; }
+		int sum_of_tree = sum_tree(root->left) + root->val + sum_tree(root->right);
+		if (sum_of_tree < minimum) {
+			minimum = sum_of_tree;
+			minimum_root = root;
+		}
+		return sum_of_tree;
+	}
+};
