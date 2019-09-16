8d2c863f8d0f05936de3befeafce478b1c4856ca
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-16 19:26:00 +0800

    recursive method and iterative method

---
+++ Binary_Tree/LintCode_902_leetcode_230_kthSmallestElementInBST/LintCode_902_leetcode_230_kthSmallestElementInBST_cpp.cpp
@@ -0,0 +1,67 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+// naive inorder traverse method, time O(n), space O(n)
+class Solution {
+public:
+	int kthSmallest(TreeNode* root, int k) {
+		if (root == nullptr) {
+			return -1;
+		}
+		vector<int> ans;
+		inorder(root, ans);
+		return ans[k - 1];
+	}
+
+	void inorder(TreeNode* root, vector<int> &ans) {
+		if (root == nullptr) {
+			return;
+		}
+		inorder(root->left, ans);
+		ans.push_back(root->val);
+		inorder(root->right, ans);
+	}
+};
+
+
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+
+// Iterative method, using stack, time O(H + k), space O(H + k)
+class Solution {
+public:
+	int kthSmallest(TreeNode* root, int k) {
+		if (root == nullptr) {
+			return -1;
+		}
+
+		stack<TreeNode*> S;
+
+		while (true) {
+			while (root != nullptr) {
+				S.push(root);
+				root = root->left;
+			}
+
+			root = S.top(); S.pop();
+			if (!(--k)) {
+				return root->val;
+			}
+
+			root = root->right;
+		}
+	}
+};
