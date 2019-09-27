5477736f7a23bc01bd4546d63223fea3d7e32ab8
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-09-27 20:07:27 +0800

    Permutations Template

---
+++ DFS_and_Recursion/Lintcode_15_leetcode_46_Permutations/Lintcode_15_leetcode_46_Permutations_cpp.cpp
@@ -0,0 +1,32 @@
+// use swap to avoid more space
+class Solution {
+public:
+	vector<vector<int>> permute(vector<int>& nums) {
+		if (nums.empty()) {
+			return{ {} };
+		}
+		vector<int> path;
+		vector<vector<int>> res;
+		dfs(nums, path, res, 0);
+		return res;
+	}
+
+
+	void dfs(vector<int> &nums, vector<int> &path, vector<vector<int>> &res, int idx) {
+		if (idx == nums.size()) {
+			res.push_back(path);
+			return;
+		}
+		if (idx > nums.size()) {
+			return;
+		}
+
+		for (int i = idx; i < nums.size(); i++) {
+			path.push_back(nums[i]);
+			swap(nums[i], nums[idx]);
+			dfs(nums, path, res, idx + 1);
+			swap(nums[i], nums[idx]);
+			path.pop_back();
+		}
+	}
+};
---
+++ DFS_and_Recursion/Lintcode_16_leetcode_47_Permutations2/Lintcode_16_leetcode_47_Permutations2_cpp.cpp
@@ -0,0 +1,43 @@
+// deduplication is the hardest thing here
+class Solution {
+public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		if (nums.empty()) {
+			return{ {} };
+		}
+		sort(nums.begin(), nums.end());
+		vector<vector<int>> res;
+		vector<int> path;
+		vector<bool> visited(nums.size(), false);
+		dfs(nums, path, res, 0, visited);
+
+		return res;
+	}
+
+	void dfs(const vector<int> &nums, vector<int> &path, vector<vector<int>> &res, int count, vector<bool> &visited) {
+		if (count == nums.size()) {
+			res.push_back(path);
+			return;
+		}
+
+		if (count > nums.size()) {
+			return;
+		}
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (visited[i] == true) {
+				continue;
+			}
+
+			if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == false) {
+				continue;
+			}
+
+			visited[i] = true;
+			path.push_back(nums[i]);
+			dfs(nums, path, res, count + 1, visited);
+			path.pop_back();
+			visited[i] = false;
+		}
+	}
+};
