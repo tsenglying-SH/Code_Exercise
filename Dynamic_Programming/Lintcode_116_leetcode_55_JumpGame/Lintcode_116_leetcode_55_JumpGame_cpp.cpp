f1cf3ba34d79dee1cbf36c1f4bfadc4b52adfd5f
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-10-02 13:43:03 +0800

    Greedy!

---
+++ Dynamic_Programming/Lintcode_116_leetcode_55_JumpGame/Lintcode_116_leetcode_55_JumpGame_cpp.cpp
@@ -0,0 +1,25 @@
+// DP method Time O(n), Space O(1)
+class Solution {
+public:
+	bool canJump(vector<int>& nums) {
+		if (nums.empty()) {
+			return false;
+		}
+
+		int max_vld = 0;
+
+		for (int i = 0; i < nums.size(); ++i) {
+			// check whether last can be reached first
+			if (max_vld >= nums.size() - 1) {
+				return true;
+			}
+			// check whether the index is valid
+			if (i > max_vld) {
+				break;
+			}
+			// try updating the max_vld
+			max_vld = max(max_vld, nums[i] + i);
+		}
+		return false;
+	}
+};
