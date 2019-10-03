4d66c703e9d053faa85cefedbc5dd4ac926115b7
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-10-03 13:35:53 +0800

    Add files via upload

---
+++ twoPointers/LintCode_5_Leetcode_215_kthLargestElementInArray/LintCode_5_Leetcode_215_kthLargestElement_cpp.cpp
@@ -0,0 +1,47 @@
+// Quick Selection Time O(n), an important template!
+class Solution {
+public:
+	/**
+	* @param n: An integer
+	* @param nums: An array
+	* @return: the Kth largest element
+	*/
+	int kthLargestElement(int n, vector<int> &nums) {
+		// write your code here
+		if (nums.empty()) {
+			return -1;
+		}
+
+		return partition(nums, 0, nums.size() - 1, n);
+	}
+
+	int partition(vector<int> &nums, int start, int end, int n) {
+		if (start >= end && n == 1) {
+			return nums[start];
+		}
+
+		int l = start, r = end;
+		int mid = l + (r - l) / 2;
+		int pivot = nums[mid];
+
+		while (l <= r) {
+			while (l <= r && nums[l] > pivot) {
+				l++;
+			}
+			while (l <= r && nums[r] < pivot) {
+				r--;
+			}
+			if (l <= r) {
+				swap(nums[l++], nums[r--]);
+			}
+		}
+
+		if (start + n - 1 >= l) {
+			return partition(nums, l, end, n - l + start);
+		}
+		else if (start + n - 1 <= r) {
+			return partition(nums, start, r, n);
+		}
+		return pivot;
+	}
+};
