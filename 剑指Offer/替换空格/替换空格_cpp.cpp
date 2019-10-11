ff271daaa33104346ae6d9aec30fae494365bb49
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-10-11 23:33:55 +0800

    when using cstring, care about memory overlap

--- 剑指Offer/替换空格/替换空格_cpp.cpp
+++ 剑指Offer/替换空格/替换空格_cpp.cpp
@@ -57,8 +57,9 @@ public:
 // Solution 3: same algorithm but use cstring, two pointers, time O(n), space O(1);
 class Solution {
 public:
+	// here length means string's length, rewrite may cause memory overlap
 	void replaceSpace(char *str, int length) {
-		if (length <= 0) {
+		if (length <= 0 || str == nullptr) {
 			return;
 		}
 		int space_cnt = 0;
@@ -68,8 +69,50 @@ public:
 			}
 		}
 		int new_len = length + 2 * space_cnt;
+
+		// here may cause memory overlap
 		// notice cstring char array end with '/0', so 1 more length
 		int left = length, right = new_len;
+
+		while (left >= 0 && right >= 0) {
+			if (str[left] != ' ') {
+				str[right--] = str[left--];
+			}
+			else {
+				--left;
+				str[right--] = '0';
+				str[right--] = '2';
+				str[right--] = '%';
+			}
+		}
+		return;
+	}
+};
+
+class Solution {
+public:
+	// if length means char array's capacity, we can avoid memory overlap, when overlap happens, just give up replacing
+	void replaceSpace(char *str, int length) {
+		if (length <= 0 || str == nullptr) {
+			return;
+		}
+		int originLength = 0, newLength = 0;
+		for (int i = 0; i < length; ++i) {
+			if (str[i] == ' ') {
+				++originLength;
+				newLength += 3;
+			}
+			else {
+				++originLength;
+				++newLength;
+			}
+		}
+		// str capacity too small, may cause memory overlap
+		if (length < newLength) {
+			return;
+		}
+		// notice cstring char array end with '/0'
+		int left = originLength, right = newLength;
 
 		while (left >= 0 && right >= 0) {
 			if (str[left] != ' ') {
