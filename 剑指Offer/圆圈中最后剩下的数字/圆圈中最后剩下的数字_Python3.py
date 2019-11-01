bf343f66d013eec80ec279c4018505b49c001e5b
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-11-01 21:23:31 +0800

    Add files via upload

--- 剑指Offer/圆圈中最后剩下的数字/圆圈中最后剩下的数字_Python3.py
+++ 剑指Offer/圆圈中最后剩下的数字/圆圈中最后剩下的数字_Python3.py
@@ -17,6 +17,21 @@ class Solution(object):
         if n == 1: return 0
         return (self.lastRemaining(n - 1, m) + m) % n
     
+# we can change it into iterative method, time O(n), space O(1)
+class Solution(object):
+    def lastRemaining(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        if n <= 0 or m <= 0: return 0
+        if n == 1: return 0
+        res = 0
+        for i in range(2, n + 1):
+            res = (res + m) % i
+        return res    
+        
 # one-base, time O(n), space O(n)
 class Solution(object):
     def lastRemaining(self, n, m):
@@ -28,3 +43,18 @@ class Solution(object):
         if n <= 0 or m <= 0: return 0
         if n == 1: return 1
         return (self.lastRemaining(n - 1, m) + m - 1) % n + 1
+    
+# we can change it into iterative method, time O(n), space O(1)
+class Solution(object):
+    def lastRemaining(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        if n <= 0 or m <= 0: return 0
+        if n == 1: return 1
+        res = 1
+        for i in range(2, n + 1):
+            res = (res + m - 1) % i + 1
+        return res   
--- 剑指Offer/圆圈中最后剩下的数字/圆圈中最后剩下的数字_cpp.cpp
+++ 剑指Offer/圆圈中最后剩下的数字/圆圈中最后剩下的数字_cpp.cpp
@@ -8,6 +8,18 @@ public:
 	}
 };
 
+// we can also change it into iterative method, time O(n), space O(1)
+class Solution {
+public:
+	int lastRemaining(int n, int m) {
+		if (n <= 0 || m <= 0) return 0;
+		if (n == 1) return 0;
+		int res = 0;
+		for (int i = 2; i <= n; ++i) res = (res + m) % i;
+		return res;
+	}
+};
+
 // one-based
 class Solution {
 public:
@@ -15,5 +27,17 @@ public:
 		if (n <= 0 || m <= 0) return 0;
 		if (n == 1) return 1;
 		return (lastRemaining(n - 1, m) + m - 1) % n + 1;
+	}
+};
+
+// we can also change it into iterative method, time O(n), space O(1)
+class Solution {
+public:
+	int lastRemaining(int n, int m) {
+		if (n <= 0 || m <= 0) return 0;
+		if (n == 1) return 1;
+		int res = 1;
+		for (int i = 2; i <= n; ++i) res = (res + m - 1) % i + 1;
+		return res;
 	}
 };
