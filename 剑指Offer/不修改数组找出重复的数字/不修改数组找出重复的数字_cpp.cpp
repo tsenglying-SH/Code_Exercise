a60963bc93b94491a3e35a848deefaf700b0c8fb
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-11-02 21:31:02 +0800

    Update better solution in cpp

--- 剑指Offer/不修改数组找出重复的数字/不修改数组找出重复的数字_cpp.cpp
+++ 剑指Offer/不修改数组找出重复的数字/不修改数组找出重复的数字_cpp.cpp
@@ -2,21 +2,22 @@
 class Solution {
 public:
 	int duplicateInArray(vector<int>& nums) {
+		if (nums.size() <= 1) return -1;
 		int start = 1, end = nums.size() - 1;
 		while (start <= end) {
-			int mid = start + ((end - start) >> 1);
+			int mid = start + (end - start) / 2;
 			int count = 0;
 			for (int num : nums) {
-				if (num <= mid) {
-					count++;
-				}
+				if (num <= mid && num >= start) ++count;
 			}
-			if (count > mid) {
+			// cout << count << endl;
+			if (count > mid - start + 1) {
 				end = mid - 1;
 			}
 			else {
 				start = mid + 1;
 			}
+			// cout << start << ' ' << end << endl;
 		}
 		return start;
 	}
